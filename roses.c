2a2a66b28d8a9cb39e0d8ab1a5a407c949ea1fc2
mburuxx <njihiamburu.3@gmail.com>
2022-11-18 04:54:24 +0300

    kick-butting the new language

---
+++ roses.c
@@ -0,0 +1,9 @@
+/* prints a string with integers present */
+#include <stdio.h>
+
+int main ()
+{
+	printf ("%d roses cost %.2f per %d\n", 24, 19.95, 12);
+	printf ("%s %d %f %c \n", "Sam", 14, -8.343524, 'X');
+	return 0;
+}
