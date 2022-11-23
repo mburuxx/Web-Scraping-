b0fb635e182c4cbbd02706d6254be3248f55b052
mburuxx <njihiamburu.3@gmail.com>
2022-11-23 07:53:50 +0300

    we may be cast to the same type temporarily but we are not the same

---
+++ data_types.c
@@ -0,0 +1,11 @@
+/* a program that uses characters, integers and floating-point numbers */
+#include <stdio.h>
+
+int main ()
+{
+	printf("I am leaning the %c programming language\n", 'C');
+	printf("Are you really %d years old?\n", 23);
+	printf("The river is only %.2f miles away\n", 0.56);
+	return 0;
+}
+
