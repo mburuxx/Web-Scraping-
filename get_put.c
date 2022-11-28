84e6c797d202bbf30ed11a16c1be43579042e3df
mburuxx <njihiamburu.3@gmail.com>
2022-11-28 23:48:09 +0300

    get them and put them in their place

---
+++ get_put.c
@@ -0,0 +1,34 @@
+/* In this program, I will demonstrate the use of getchar and putchar */
+// initiate variables
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char message[30];
+  int i;
+  
+  // print a statement to the user for instructions
+  printf("Type not more than 25 characters then press Enter... \n");
+  
+  // iterate through the variable so as to get the characters
+  for (i= 0; i < strlen(message); i++)
+    {
+      message[i] = getchar();
+      //terminate the loop
+      if (i == '\n')
+	{
+	  i--;
+	  break;
+	}
+    }
+  putchar('\n');
+  // iterate through the variable to output the characters
+  for ( ; i < strlen(message); i--)
+    {
+      putchar(message[i]);
+    }
+  putchar('\n');
+
+  return 0; //terminate program
+}
