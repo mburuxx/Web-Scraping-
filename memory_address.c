d4e24342b554a3f03163e0c90efcb546ceda0631
mburuxx <njihiamburu.3@gmail.com>
2022-12-19 08:14:59 +0300

    Point to b that points to c

--- memory_address.c
+++ memory_address.c
@@ -8,11 +8,15 @@
 
 int main(void)
 {
-	int c;
+	int c, *p;
 	char i;
+
+	c = 24;
+	p = &c;
 
 	printf("The address of the 'int' is at: %p\n", &c);
 	printf("The address of the 'char' is at: %p\n", &i);
+	printf("The value of the variable 'p' is : %p\n", p);
 
 	return (0);
 }
